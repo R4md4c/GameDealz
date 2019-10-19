1655a78d33b52cc0c3b46a2666fa6d400f9b8a8b
Ahmed I. Khalil <ahmedibrahimkhali@gmail.com>
2019-10-19 23:44:20 +0200

    Upgrade Kotshi

--- network/src/main/java/de/r4md4c/gamedealz/network/model/steam/ResponseWrapper.kt
+++ network/src/main/java/de/r4md4c/gamedealz/network/model/steam/ResponseWrapper.kt
@@ -20,4 +20,4 @@ package de.r4md4c.gamedealz.network.model.steam
 import se.ansman.kotshi.JsonSerializable
 
 @JsonSerializable
-class ResponseWrapper<T>(val success: Boolean, val data: T?)
+data class ResponseWrapper<T>(val success: Boolean, val data: T?)
